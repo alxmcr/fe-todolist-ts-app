5846d11638b4f70a1790d2d871c8444c71d1c53e
Alejandro M. Coca <alxmcr@outlook.com>
2021-06-18 19:30:47 -0400

    Styles

--- src/components/TodoListCard/index.tsx
+++ src/components/TodoListCard/index.tsx
@@ -13,7 +13,13 @@ export const TodoListCard = () => {
             </div>
             <div className="todolist__footer">
                 <form className="todolist__form">
-                    <input type="text" className="todolist__input" placeholder="Enter the task's text" />
+                    <label htmlFor="text">Description:</label>
+                    <input type="text"
+                        className="todolist__input"
+                        placeholder="Enter the task's description"
+                        id="text"
+                        name="text"
+                    />
                     <button type="submit" className="todolist__button">
                         <i className="fas fa-plus todolist__icon--plus"></i>
                         <span className="todolist__btntext">ADD NEW</span>
