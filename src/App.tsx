2cdcfb1c18cbe5e52a346cbbab94c7534956550a
Alejandro M. Coca <alxmcr@outlook.com>
2021-06-29 16:37:30 -0400

    Improvement with styles and add attributions

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <TodoListCard />
-      <small>Icons by Font-Aweasome, and favicon by Adri Ansyah.</small>
-    </>
+      <small className="App__attribution">Thanks! Icons by Font-Aweasome, favicon by Adri Ansyah, and Photo by Glenn Carstens-Peters on Unsplash.</small>
+    </div>
   );
 }
 
